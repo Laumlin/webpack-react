1269ad272fb25985f49e215104f79e51b9d96546
郑康霖 <1255964530@qq.com>
2018-11-25 16:36:43 +0800

    配置sass-loader，自定义样式表使用scss模块化，外部样式表使用css不启用模块化

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,21 @@ module.exports = {
 			use: 'babel-loader', //配置babel-loader解析.js或.jsx结尾的文件
 			exclude: /node_modules/
 		}, {
+			// 可以在css-loader后，通过？追加参数
+			// 其中 有个固定参数，叫做 modules， 表示 普通的css样式表，启用模块化
+			// 自己规定： 第三方 的样式表都是以.css结尾，这样我们不要为普通的 .css 启用模块化
+			//					 自己的样式表，都要以 .scss 或 .less结尾， 只为 .scss 或 .less 文件启用模块化
 			test: /\.css$/,
 			use: ['style-loader', 'css-loader']
+		}, {
+			test: /.scss$/,
+			/*使用localIdentName自定义生成的类名格式，可选参数有：
+			[path]：表示样式表相 对于项目根目录所在 路径
+			[name]：表示 样式表 文件名称
+			[local] ：表示样式的类名定义名称
+			[hash:length]：表示默认32为的hash值，取5-6位即可，防止类名重复
+			*/
+			use: ['style-loader', 'css-loader?modules&localIdentName=[path][name]-[local]-[hash:6]', 'sass-loader']
 		}]
 	},
 
