e15bac88a9e7a4bb9210c17bef97d8dd422df10b
郑康霖 <1255964530@qq.com>
2018-12-04 23:21:00 +0800

    初始化store仓库

---
+++ src/store/reducer.js
@@ -0,0 +1,19 @@
+const defaultState = {
+	comList: [{
+		id: 0,
+		name: '张三',
+		comment: '哈哈哈'
+	}, {
+		id: 1,
+		name: '张三',
+		comment: '哈哈哈'
+	}, {
+		id: 2,
+		name: '张三',
+		comment: '哈哈哈'
+	}]
+}
+
+export default (state = defaultState, action) => {
+	return state
+}
